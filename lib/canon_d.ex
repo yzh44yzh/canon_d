957577c0f5585e78e1562757c53d1d5640670b17
Yuriy Zhloba <y.zhloba@erlyvideo.org>
2025-07-23 22:38:54 +0300

    parse deck

--- lib/canon_d.ex
+++ lib/canon_d.ex
@@ -12,10 +12,46 @@ defmodule CanonD do
 
   @spec learn_deck(String.t()) :: :ok
   def learn_deck(deck) do
-    IO.puts(deck)
+    parse_deck(deck)
+    |> dbg()
     # TODO: 
-    # - parse cards
     # - learn card
     :ok
+  end
+
+  # TODO spec and test
+  def parse_deck(deck) do
+    deck
+    |> String.split("\n")
+    |> Enum.map(&String.trim/1)
+    |> Enum.filter(fn line -> line != "" and line != "```" end)
+    |> group_by_header()
+    # TODO filter by Card.has_lines?
+    |> Enum.filter(fn 
+      ([_single_line]) -> false;
+      ([_|_]) -> true
+    end)
+    # TODO not needed
+    |> Enum.map(fn lines -> 
+      [header | lines] = Enum.reverse(lines)
+      {header, lines}
+    end)
+  end
+
+  # TODO spec and test
+  def group_by_header([first_line | lines]) do
+    Enum.reduce(
+      lines,
+      {[], [first_line]}, 
+      fn line, {cards, curr_card} ->
+        if String.starts_with?(line, "#") do
+          # TODO create Card struct
+          {[curr_card | cards], [line]}
+        else
+          # TODO add line to Card struct
+          {cards, [line | curr_card]}
+        end
+      end)
+    |> then(fn {cards, last_card} -> [last_card | cards] end)
   end
 end
